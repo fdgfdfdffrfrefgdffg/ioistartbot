9e3e174a34fdfd8316d67761c28fd6f97733e3a2
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-10-26 20:32:59 +0500

    commit

--- message/more.py
+++ message/more.py
@@ -3,20 +3,15 @@ from sqldata import del_user
 from aiogram.enums import ChatType
 from aiogram.fsm.context import FSMContext
 from keyboards.inline import sub_channels_markup
-from aiogram.types import InputFile
+from aiogram.types import FSInputFile
 import os
 
-DB_FILE_PATH = "data.db"
 
 async def send_db_file(message: Message):
-    if message.from_user.id == 5165396993:
+    if message.from_user.id != 5165396993:
         return
-    if os.path.exists(DB_FILE_PATH):
-        db_file = InputFile(DB_FILE_PATH)
-        await message.answer_document(db_file, caption="Mana, data.db fayli.")
-    else:
-        await message.answer("data.db fayli topilmadi.")
-
+    await message.answer_document(FSInputFile("data.db"), caption="Mana, data.db fayli.")
+    
 async def delete_user_answer(message: Message):
     user_id = message.text.split(" ")[1]
     del_user(int(user_id))
