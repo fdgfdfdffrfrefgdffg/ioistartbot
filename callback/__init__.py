ab369c346ac12a3e114df0d201f60355dc957dd4
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-10-27 20:00:14 +0500

    commit

--- callback/__init__.py
+++ callback/__init__.py
@@ -7,4 +7,4 @@ router = Router()
 
 router.callback_query.register(signup.get_region_answer, FirstCupdownStates.region)
 router.callback_query.register(signup.get_district_answer, FirstCupdownStates.district)
-router.callback_query.register(more.sub_channels_answer, F.data == "checksub")
+router.callback_query.register(more.sub_channels_answer, F.data.startswith("checksub"))
